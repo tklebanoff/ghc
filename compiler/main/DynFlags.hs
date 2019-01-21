ca4751366b074e52ff8d06298593f9193d9f7080
Joachim Breitner <mail@joachim-breitner.de>
2019-01-20 19:25:26 -0500

    Make tablesNextToCode a proper dynamic flag (#15548)
    Summary:
    There is no more use of the TABLES_NEXT_TO_CODE CPP macro in
    `compiler/`. GHCI_TABLES_NEXT_TO_CODE is also removed entirely. The
    default value of `tablesNextToCode` is calculated as before, but now
    users of the GHCI API can modify this flag.
    
    That said, GHC still is hardcoded to define TABLES_NEXT_TO_CODE based on
    that default value. This is bad, but neccessary until the remaining uses
    of TABLES_NEXT_TO_CODE get it from make/Hadrian.
    
    Reviewers:
    
    Subscribers: TerrorJack, rwbarton, carter
    
    GHC Trac Issues: #15548
    
    Differential Revision: https://phabricator.haskell.org/D5082

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -58,7 +58,6 @@ module DynFlags (
         fFlags, fLangFlags, xFlags,
         wWarningFlags,
         dynFlagDependencies,
-        tablesNextToCode,
         makeDynFlagsConsistent,
         shouldUseColor,
         shouldUseHexWordLiterals,
@@ -878,6 +877,10 @@ data DynFlags = DynFlags {
   integerLibrary        :: IntegerLibrary,
     -- ^ IntegerGMP or IntegerSimple. Set at configure time, but may be overriden
     --   by GHC-API users. See Note [The integer library] in PrelNames
+  tablesNextToCode      :: Bool,
+    -- ^ Determines whether we will be compiling info tables that reside just
+    --   before the entry code, or with an indirection to the entry code. See
+    --   TABLES_NEXT_TO_CODE in includes/rts/storage/InfoTables.h.
   llvmTargets           :: LlvmTargets,
   llvmPasses            :: LlvmPasses,
   verbosity             :: Int,         -- ^ Verbosity level: see Note [Verbosity levels]
@@ -1619,18 +1622,6 @@ defaultObjectTarget platform
   | cGhcWithNativeCodeGen == "YES"      =  HscAsm
   | otherwise                           =  HscLlvm
 
-tablesNextToCode :: DynFlags -> Bool
-tablesNextToCode dflags
-    = mkTablesNextToCode (platformUnregisterised (targetPlatform dflags))
-
--- Determines whether we will be compiling
--- info tables that reside just before the entry code, or with an
--- indirection to the entry code.  See TABLES_NEXT_TO_CODE in
--- includes/rts/storage/InfoTables.h.
-mkTablesNextToCode :: Bool -> Bool
-mkTablesNextToCode unregisterised
-    = not unregisterised && cGhcEnableTablesNextToCode == "YES"
-
 data DynLibLoader
   = Deployable
   | SystemDependent
@@ -1879,6 +1870,9 @@ defaultDynFlags mySettings (myLlvmTargets, myLlvmPasses) =
         ghcLink                 = LinkBinary,
         hscTarget               = defaultHscTarget (sTargetPlatform mySettings),
         integerLibrary          = cIntegerLibraryType,
+        tablesNextToCode        =
+            not (platformUnregisterised $ sTargetPlatform mySettings) &&
+            cGhcEnableTablesNextToCode == "YES",
         verbosity               = 0,
         optLevel                = 0,
         debugLevel              = 0,
