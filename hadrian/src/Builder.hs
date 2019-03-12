f0b315b95ed18b31f6134f8bad3dc914ac8d59fb
Matthew Pickering <matthewtpickering@gmail.com>
2019-03-12 11:30:54 +0000

    WIP: Use system cabal to install user packages

--- hadrian/src/Builder.hs
+++ hadrian/src/Builder.hs
@@ -110,6 +110,7 @@ data Builder = Alex
              | Autoreconf FilePath
              | DeriveConstants
              | Cabal ConfigurationInfo Stage
+             | SysCabal FilePath
              | Cc CcMode Stage
              | Configure FilePath
              | GenApply
@@ -290,6 +291,7 @@ systemBuilderPath builder = case builder of
     Cc  _  _        -> fromKey "cc"
     -- We can't ask configure for the path to configure!
     Configure _     -> return "configure"
+    SysCabal _   -> fromKey "system-cabal"
     Ghc _  Stage0   -> fromKey "system-ghc"
     GhcPkg _ Stage0 -> fromKey "system-ghc-pkg"
     Happy           -> fromKey "happy"
